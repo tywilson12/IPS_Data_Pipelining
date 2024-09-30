2e6888fb1dcb182c97b3144fb2455036d0e452fd
tywilson12 <tyler.p.wilson12@gmail.com>
2024-09-30 15:57:42 +0000

    updated function and plots

--- STFT.py
+++ STFT.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import stft
+from scipy.signal import stft, butter, filtfilt, decimate
 import glob
 from tqdm import tqdm
 from functions.hdas_class import HDAS
@@ -14,14 +14,30 @@ INSERT FILEPATH AND DURATION IN SECONDS BELOW
 """
 
 filepath = "AK_Data/2022_05_05_20*"
-duration_seconds = 600
-starting_seconds = 600
-freq_range = (1, 10)
-channel_range = None # optional
+duration_seconds = 180
+starting_seconds = 780
+freq_range = (0.5, 10)  # Frequency range for band-pass filter
+channel_range = None  # optional
+decimation_factor = None
 
-def calculate_band_power_stft(data, sampling_rate, freq_range, nperseg=100): # May need to modify nperseg based on segment length
+def bandpass_filter(data, sampling_rate, freq_range, order=5):
 
-    freqs, times, Zxx = stft(data, fs=sampling_rate, nperseg=nperseg, noverlap=nperseg//2)
+    nyquist = 0.5 * sampling_rate
+    low = freq_range[0] / nyquist
+    high = freq_range[1] / nyquist
+    b, a = butter(order, [low, high], btype='band')
+    filtered_data = filtfilt(b, a, data)
+    return filtered_data
+
+def calculate_band_power_stft(data, sampling_rate, freq_range, nperseg=100): 
+
+    if decimation_factor != None:
+        data = decimate(data, decimation_factor, axis=-1, zero_phase=True).astype(np.float32)
+        sampling_rate = sampling_rate/decimation_factor
+
+    filtered_data = bandpass_filter(data, sampling_rate, freq_range)
+    
+    freqs, times, Zxx = stft(filtered_data, fs=sampling_rate, nperseg=nperseg, noverlap=nperseg//2)
     
     freq_indices = np.where((freqs >= freq_range[0]) & (freqs <= freq_range[1]))[0]
     
@@ -29,23 +45,8 @@ def calculate_band_power_stft(data, sampling_rate, freq_range, nperseg=100): # M
     
     return band_power, times
 
-def hdas_denoising(data, threshold=1000, window_size=3, strain_max=5000, strain_min=-5000):
-    denoised_data = data.copy()
-    
-    denoised_data[denoised_data > strain_max] = 0
-    denoised_data[denoised_data < strain_min] = 0
-    
-    zero_mask = (denoised_data == 0)
-
-    filtered_data = uniform_filter1d(denoised_data, size=window_size, axis=1)
-
-    denoised_data[zero_mask] = filtered_data[zero_mask]
-    
-    return denoised_data
-
 def plot_band_power_heatmap_stft(data, sampling_rate, duration_seconds, starting_seconds, freq_range, channel_range=None):
-
-    segment_length = 5  # Length of each segment in seconds
+    segment_length = 1  # Length of each segment in seconds
     segment_samples = int(segment_length * sampling_rate)
     sample_start = int(starting_seconds * sampling_rate)
     total_samples = int(sampling_rate * duration_seconds)
@@ -55,15 +56,10 @@ def plot_band_power_heatmap_stft(data, sampling_rate, duration_seconds, starting
     
     if channel_range == None:
         num_channels = data.shape[0]
+        heatmap_data = data[:, sample_start:(sample_start + total_samples)]
     else:
-        num_channels = channel_range[1]-channel_range[0]
-    
-    strain_max = 5000
-    strain_min = -5000
-
-    print("Denoising Data...")
-
-    heatmap_data = hdas_denoising(data[:, sample_start:(sample_start + total_samples)])
+        num_channels = channel_range[1] - channel_range[0]
+        heatmap_data = data[channel_range[0]:channel_range[1], sample_start:(sample_start + total_samples)]
 
     power_matrix = []
 
@@ -73,7 +69,7 @@ def plot_band_power_heatmap_stft(data, sampling_rate, duration_seconds, starting
         power_matrix.append(band_power)
 
     power_matrix = np.array(power_matrix)
-    
+
     if duration_seconds > 60:
         times = times / 60.0
         time_label = 'Time (minutes)'
@@ -81,36 +77,39 @@ def plot_band_power_heatmap_stft(data, sampling_rate, duration_seconds, starting
         time_label = 'Time (seconds)'
 
     plt.figure(figsize=(12, 8))
+    vmin = np.percentile(power_matrix, 0)
+    vmax = np.percentile(power_matrix, 80)
 
-    vmin = np.percentile(power_matrix, 50)
-    vmax = np.percentile(power_matrix, 99)
-
-    sns.heatmap(power_matrix, cmap='viridis', cbar=True, xticklabels=int(sampling_rate * segment_length),
+    sns.heatmap(power_matrix, cmap='Purples', cbar=True, xticklabels=int(sampling_rate * segment_length),
                 cbar_kws={'label': 'Power in Freq. Band'}, vmin=vmin, vmax=vmax)
-
 
     plt.xlabel(time_label)
     plt.ylabel('Channel')
     plt.title(f'Power in {freq_range[0]}-{freq_range[1]} Hz Band (STFT)')
-    plt.xticks(ticks=np.arange(0, len(times), len(times)//10), labels=np.round(times[np.arange(0, len(times), len(times)//10)], 2))
+    plt.xticks(ticks=np.arange(0, len(times), len(times)//(duration_seconds//60)), labels=np.round(times[np.arange(0, len(times), len(times)//(duration_seconds//60))], 2))
     plt.yticks(ticks=np.arange(0, num_channels, 500), labels=np.arange(0, num_channels, 500))
     plt.gca().invert_yaxis()
-
 
     output_dir = "output"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     plt.savefig(os.path.join(output_dir, "band_power_heatmap_stft.png"))
     plt.close()
-    print("Plot Saved...")
+    print("Plot Saved as band_power_heatmap_stft.png...")
 
-def plot_hdas_from_file(filepath, duration_seconds, starting_seconds, channel_range = None):
+def plot_hdas_from_file(filepath, duration_seconds, starting_seconds, channel_range=None):
 
     bins = np.sort(glob.glob(filepath))
 
     if len(bins) == 1:
 
         hdas_data = HDAS(bins[0], load=True)
+
+        [hdas_data.Data, HDAS_LaserRef, aditional_out] = laserDenoisingRefFiberv2(data=hdas_data,
+                                                                              FiberRefStop=hdas_data.FiberRefStop,
+                                                                              FiberRefStart=hdas_data.FiberRefStart,
+                                                                              RawData=hdas_data.RawMode
+                                                                              )
 
         plot_band_power_heatmap_stft(hdas_data.Data, 60000 / (10 * 60), duration_seconds, starting_seconds, freq_range, channel_range)
 
@@ -119,6 +118,12 @@ def plot_hdas_from_file(filepath, duration_seconds, starting_seconds, channel_ra
 
             hdas_data = HDAS(bins[i], load=True)
 
+            [hdas_data.Data, HDAS_LaserRef, aditional_out] = laserDenoisingRefFiberv2(data=hdas_data,
+                                                                              FiberRefStop=hdas_data.FiberRefStop,
+                                                                              FiberRefStart=hdas_data.FiberRefStart,
+                                                                              RawData=hdas_data.RawMode
+                                                                              )
+
             if i == 0:
                 combined_data = hdas_data.Data
             else:
@@ -126,4 +131,4 @@ def plot_hdas_from_file(filepath, duration_seconds, starting_seconds, channel_ra
         
         plot_band_power_heatmap_stft(combined_data, 60000 / (10 * 60), duration_seconds, starting_seconds, freq_range, channel_range)
 
-plot_hdas_from_file(filepath, duration_seconds, starting_seconds)
+plot_hdas_from_file(filepath, duration_seconds, starting_seconds, channel_range)
